18932f6b1752ee9e7dc6bc4994e1254b0839e52b
13aksh <1392aksh@gmail.com>
2016-07-13 14:27:16 +0530

    Just before the templates, DB connect and DB API done

--- music/views.py
+++ music/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album, Song
 
 # Create your views here.
 def index(request):
-	html = "This is the first page"
+	all_albums = Album.objects.all()
+	html = "This is the first page<br>"
+	for album in all_albums:
+		url = '/music/' + str(album.id) + '/'
+		html += "<a href='" + url + "'>" + album.album_title + " - " + album.artist + "</a><br>"
+	return HttpResponse(html)
+
+def details(request, album_id):
+	html = "<h1>This is Album no. : " + str(album_id) + "</h1>"
 	return HttpResponse(html)
 
